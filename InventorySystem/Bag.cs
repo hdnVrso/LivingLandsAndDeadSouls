b7771f13bf08ea8d9713a2ca1035c5974bdf12a0
boyboyjoy <moximmorozov@yandex.ru>
2020-06-26 10:55:36 +0300

    destroy item, fixed change sprites system, modify authenticate user info

--- InventorySystem/Bag.cs
+++ InventorySystem/Bag.cs
@@ -1,5 +1,6 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
+using InventorySystem;
 using UnityEngine;
 using UnityEngine.UI;
 
@@ -8,6 +9,8 @@ public class Bag : MonoBehaviour
 
     void Start()
     {
+        _gunSlot = GameObject.Find("GunSlot");
+        _slotSuit = GameObject.Find("SuitSlot");
         _closeButton = GameObject.Find("CloseButton").GetComponent<ActiveController>();
         _playerInventory = GameObject.Find("Player").GetComponent<Inventory>();
         _skills = GameObject.Find("Skills");
@@ -20,6 +23,8 @@ public class Bag : MonoBehaviour
             {
                 bagElement.SetActive(true);
                 _skills.SetActive(true);
+                _slotSuit.SetActive(true);
+                _gunSlot.SetActive(true);
             }
             _isClosed = false;
         }
@@ -29,6 +34,8 @@ public class Bag : MonoBehaviour
                 bagElement.SetActive(false);
                 _skills.SetActive(false);
                 _closeButton.Disabled();
+                _slotSuit.SetActive(false);
+                _gunSlot.SetActive(false);
             }
             _isClosed = true;
         }
@@ -39,5 +46,7 @@ public class Bag : MonoBehaviour
     private bool _isClosed;
     private Inventory _playerInventory;
     private GameObject _skills;
+    private GameObject _gunSlot;
+    private GameObject _slotSuit;
 }
 
---
+++ InventorySystem/DestroyItem.cs
@@ -0,0 +1,83 @@
+﻿using UnityEngine;
+using System.Collections;
+using InventorySystem;
+using UnityEngine.Rendering;
+using UnityEngine.UI;
+
+public class DestroyItem : MonoBehaviour
+{
+    public GameObject _destroyPanel;
+    private float doubleClickTimeLimit = 0.25f;
+
+    protected void Start()
+    {
+        StartCoroutine(InputListener());
+    }
+
+// Update is called once per frame
+    private IEnumerator InputListener() 
+    {
+        while(enabled)
+        { //Run as long as this is activ
+
+            if(Input.GetMouseButtonDown(0))
+                yield return ClickEvent();
+
+            yield return null;
+        }
+    }
+
+    private IEnumerator ClickEvent()
+    {
+        //pause a frame so you don't pick up the same mouse down event.
+        yield return new WaitForEndOfFrame();
+
+        float count = 0f;
+        while(count < doubleClickTimeLimit)
+        {
+            if(Input.GetMouseButtonDown(0))
+            {
+                DoubleClick();
+                yield break;
+            }
+            count += Time.deltaTime;// increment counter by change in time between frames
+            yield return null; // wait for the next frame
+        }
+        SingleClick();
+    }
+
+
+    private void SingleClick()
+    {
+        GunSelect gunSelect;
+        OutfitSelect outfitSelect;
+        HealthBoost healthBoost;
+        gunSelect = gameObject.GetComponent<GunSelect>();
+        outfitSelect = gameObject.GetComponent<OutfitSelect>();
+        healthBoost = gameObject.GetComponent<HealthBoost>();
+        if (gunSelect != null)
+        {
+            Debug.Log("GunSelect use");
+            gunSelect.Use();
+        }
+        else if (outfitSelect != null)
+        {
+            Debug.Log("OutFit select use");
+            outfitSelect.Use();
+        }
+        else if (healthBoost != null)
+        {
+            Debug.Log("HealthBoos use");
+            healthBoost.Use();
+        }
+    }
+
+    private void DoubleClick()
+    {
+        _destroyPanel = GameObject.Find("DestroyPanel");
+        Debug.Log("Set active destroy panel");
+        _destroyPanel.SetActive(true);
+        _destroyPanel.GetComponent<DestroyingItem>().destroyingItem = gameObject;
+    }
+
+}
